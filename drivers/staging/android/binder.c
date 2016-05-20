487455fc9a9ae3ecf56746ad5a8d5d6165aed98d
Patrick Tjin <pattjin@google.com>
2016-05-20 16:15:24 +0800

    android: binder: move preempt disable after mmput
    Don't disable preemption until after mmput since
    mmput might sleep.
    
    Bug: 24452701
    
    Change-Id: I1b5e636ae039291721dea3c294a5d16a0fabb7c6
    Mot-CRs-fixed:(CR)
    Signed-off-by: Patrick Tjin <pattjin@google.com>
    Reviewed-on: http://gerrit.mot.com/857884
    SLTApproved: Slta Waiver <sltawvr@motorola.com>
    SME-Granted: SME Approvals Granted
    Tested-by: Jira Key <jirakey@motorola.com>
    Reviewed-by: Yi-Wei Zhao <gbjc64@motorola.com>
    Submit-Approved: Jira Key <jirakey@motorola.com>

--- drivers/staging/android/binder.c
+++ drivers/staging/android/binder.c
@@ -622,6 +622,7 @@ static int binder_update_page_range(struct binder_proc *proc, int allocate,
 		up_write(&mm->mmap_sem);
 		mmput(mm);
 	}
+	preempt_disable();
 	return 0;
 
 free_range:
@@ -644,6 +645,7 @@ err_no_vma:
 		up_write(&mm->mmap_sem);
 		mmput(mm);
 	}
+	preempt_disable();
 	return -ENOMEM;
 }
 
